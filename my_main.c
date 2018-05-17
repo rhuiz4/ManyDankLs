0838bf66c8278d889f6cf87cb6d9b760158b106d
Rihui Zheng <rzheng4@stuy.edu>
2018-05-17 01:02:22 -0400

    test

--- my_main.c
+++ my_main.c
@@ -54,10 +54,10 @@ void my_main() {
   int i;
   struct matrix *tmp;
   struct stack *systems;
-  screen t;
+  screen s;
   zbuffer zb;
-  color g;
-  double step_3d = 20;
+  color c;
+  double step = 20;
   double theta;
 
   //Lighting values here for easy access
@@ -98,10 +98,161 @@ void my_main() {
 
   systems = new_stack();
   tmp = new_matrix(4, 1000);
-  clear_screen( t );
+  clear_screen( s );
   clear_zbuffer(zb);
-  g.red = 0;
-  g.green = 0;
-  g.blue = 0;
+  c.red = 0;
+  c.green = 0;
+  c.blue = 0;
+    
 
+  for (i = 0; i < lastop; i++){
+    if (op[i].opcode == PUSH){
+      //printf("PUSH\b");
+      push(systems);
+    }//end of push
+    
+    else if ( op[i].opcode == POP){
+      //printf("PUSH\b");
+      pop(systems);
+    }//end of push
+    
+    else if ( op[i].opcode == BOX){
+      //fgets(line, sizeof(line), f);
+      //printf("BOX\t%s", line);
+      /*
+      sscanf(line, "%lf %lf %lf %lf %lf %lf",
+	     xvals, yvals, zvals,
+	     xvals+1, yvals+1, zvals+1);
+      add_box(polygons, xvals[0], yvals[0], zvals[0],
+                    xvals[1], yvals[1], zvals[1]);
+      */
+      add_box(tmp, op[i].op.box.d0[0],op[i].op.box.d0[1], op[i].op.box.d0[2], op[i].op.box.d1[0],op[i].op.box.d1[1], op[i].op.box.d1[2]);
+
+      matrix_mult(peek(systems), tmp);
+      draw_polygons(tmp, s, zb,
+		    view, light, ambient, areflect, dreflect, sreflect);
+      tmp->lastcol = 0;
+    }//end of box
+    
+    else if ( op[i].opcode == SPHERE){
+      //fgets(line, sizeof(line), f);
+      //printf("SPHERE\t%s", line);
+      /*
+      sscanf(line, "%lf %lf %lf %lf",
+	     xvals, yvals, zvals, &r);
+      add_sphere( polygons, xvals[0], yvals[0], zvals[0], r, step_3d);
+      */
+      add_sphere(tmp, op[i].op.sphere.d[0], op[i].op.sphere.d[1], op[i].op.sphere.d[2], op[i].op.sphere.r, step);
+      
+      matrix_mult(peek(systems), tmp);
+      draw_polygons(tmp, s, zb,
+		    view, light, ambient, areflect, dreflect, sreflect);
+      tmp->lastcol = 0;
+    }//end of sphere
+    
+    else if ( op[i].opcode == TORUS){
+      //fgets(line, sizeof(line), f);
+      //printf("torus\t%s", line);
+      /*
+      sscanf(line, "%lf %lf %lf %lf %lf",
+	     xvals, yvals, zvals, &r, &r1);
+      add_torus(polygons, xvals[0], yvals[0], zvals[0], r, r1, step_3d);
+      */
+      add_torus(tmp, op[i].op.torus.d[0], op[i].op.torus.d[1], op[i].op.torus.d[2], op[i].op.torus.r0,op[i].op.torus.r1, step);
+
+      matrix_mult(peek(systems), tmp);
+      draw_polygons(tmp, s, zb,
+		    view, light, ambient, areflect, dreflect, sreflect);
+      tmp->lastcol = 0;
+    }//end of torus
+        
+    else if ( op[i].opcode == LINE){
+      //fgets(line, sizeof(line), f);
+      //printf("LINE\t%s", line);
+      /*
+      sscanf(line, "%lf %lf %lf %lf %lf %lf",
+	     xvals, yvals, zvals,
+	     xvals+1, yvals+1, zvals+1);
+      printf("%lf %lf %lf %lf %lf %lf",
+	xvals[0], yvals[0], zvals[0],
+             xvals[1], yvals[1], zvals[1]) 
+      add_edge(edges, xvals[0], yvals[0], zvals[0],
+	       xvals[1], yvals[1], zvals[1]);
+      */
+      add_edge(tmp, op[i].op.line.p0[0],op[i].op.line.p0[1], op[i].op.line.p0[1], op[i].op.line.p1[0],op[i].op.line.p1[1], op[i].op.line.p1[1]);
+      
+      matrix_mult(peek(systems), tmp);
+      draw_lines(tmp, s, zb, c);
+      tmp->lastcol = 0;
+    }//end line
+    
+    else if ( op[i].opcode == SCALE){
+      //fgets(line, sizeof(line), f);
+      //printf("SCALE\t%s", line);
+      //sscanf(line, "%lf %lf %lf",
+      //     xvals, yvals, zvals);
+      /* printf("%lf %lf %lf\n", */
+      /*     xvals[0], yvals[0], zvals[0]); */
+      //tmp = make_scale( xvals[0], yvals[0], zvals[0]);
+      tmp = make_scale(op[i].op.scale.d[0], op[i].op.scale.d[1], op[i].op.scale.d[2]);
+
+      matrix_mult(peek(systems), tmp);
+      copy_matrix(tmp, peek(systems));
+    }//end scale
+    
+    else if ( op[i].opcode == MOVE){
+      //fgets(line, sizeof(line), f);
+      //printf("MOVE\t%s", line);
+      //sscanf(line, "%lf %lf %lf",
+      //     xvals, yvals, zvals);
+      /* printf("%lf %lf %lf\n", */
+      /* xvals[0], yvals[0], zvals[0]); */
+      //tmp = make_translate( xvals[0], yvals[0], zvals[0]);
+      tmp = make_translate(op[i].op.move.d[0], op[i].op.move.d[1], op[i].op.move.d[2]);
+      
+      matrix_mult(peek(systems), tmp);
+      copy_matrix(tmp, peek(systems));
+    }//end translate
+    
+    else if ( op[i].opcode == ROTATE){
+      //fgets(line, sizeof(line), f);
+      //printf("Rotate\t%s", line);
+      //sscanf(line, "%c %lf",
+      //      	     &axis, &theta);
+      /* printf("%c %lf\n", */
+      /* axis, theta); */
+      theta = op[i].op.rotate.degrees * (M_PI / 180);
+      /*if ( axis == 'x' )
+	tmp = make_rotX( theta );
+      else if ( axis == 'y' )
+	tmp = make_rotY( theta );
+      else
+	tmp = make_rotZ( theta );
+      */
+      if (op[i].op.rotate.axis == 0) //x axis
+	tmp = make_rotX( theta ); 
+      else if (op[i].op.rotate.axis == 1) //y axis
+	tmp = make_rotY( theta );
+      else //z axis
+	tmp = make_rotZ( theta );
+      matrix_mult(peek(systems), tmp);
+      copy_matrix(tmp, peek(systems));
+    }//end rotate
+    
+    else if ( op[i].opcode == DISPLAY){
+      //printf("DISPLAY\t%s", line);
+      display( s );
+    }//end display
+    
+    else if ( op[i].opcode == SAVE){
+      //fgets(line, sizeof(line), f);
+      //*strchr(line, '\n') = 0;
+      //printf("SAVE\t%s\n", line);
+      save_extension(s, op[i].op.save.p->name);
+    }//end save
+  }
+  free_stack(systems);
+  free(*s);
+  free(*zb);
+  free(tmp);
 }
